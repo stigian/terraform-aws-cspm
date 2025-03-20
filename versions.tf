16dfd357dd2698f172687f00acaacdc2e92f8a64
lancehampton <lance@stigian.com>
2025-03-20 14:23:31 -0400

    update providers

--- versions.tf
+++ versions.tf
@@ -1,9 +1,15 @@
 terraform {
-  required_version = ">= 1.9.0" # OpenTofu version
+  required_version = ">= 1.8.0" # OpenTofu version
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 5.0.0"
+      configuration_aliases = [
+        aws.log,
+        aws.audit,
+        aws.management,
+        aws.hubandspoke,
+      ]
     }
     random = {
       source  = "random"
