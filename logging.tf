f93bc304a642bb76326a611e7f79185f068d0bd0
lancehampton <lance@stigian.com>
2025-03-24 17:28:51 -0400

    combine replication rules

--- logging.tf
+++ logging.tf
@@ -706,42 +706,42 @@ data "aws_iam_policy_document" "cloudtrail_log_delivery" {
   }
 }
 
-resource "aws_s3_bucket_replication_configuration" "org_cloudtrail_logs" {
-  provider = aws.log
+# resource "aws_s3_bucket_replication_configuration" "org_cloudtrail_logs" {
+#   provider = aws.log
 
-  role   = aws_iam_role.org_logs_to_hubandspoke.arn
-  bucket = data.aws_s3_bucket.ct_logs.id
+#   role   = aws_iam_role.org_logs_to_hubandspoke.arn
+#   bucket = data.aws_s3_bucket.ct_logs.id
 
-  rule {
-    id     = "org-cloudtrail"
-    status = "Enabled"
+#   rule {
+#     id     = "org-cloudtrail"
+#     status = "Enabled"
 
-    destination {
-      bucket        = module.s3_org_cloudtrail_logs.s3_bucket_arn
-      storage_class = "STANDARD"
-      encryption_configuration {
-        replica_kms_key_id = aws_kms_key.hubandspoke_s3.arn
-      }
-    }
+#     destination {
+#       bucket        = module.s3_org_cloudtrail_logs.s3_bucket_arn
+#       storage_class = "STANDARD"
+#       encryption_configuration {
+#         replica_kms_key_id = aws_kms_key.hubandspoke_s3.arn
+#       }
+#     }
 
-    source_selection_criteria {
-      replica_modifications {
-        status = "Enabled"
-      }
-      sse_kms_encrypted_objects {
-        status = "Enabled"
-      }
-    }
+#     source_selection_criteria {
+#       replica_modifications {
+#         status = "Enabled"
+#       }
+#       sse_kms_encrypted_objects {
+#         status = "Enabled"
+#       }
+#     }
 
-    filter {
-      prefix = "${var.aws_organization_id}/AWSLogs/${var.aws_organization_id}/*/CloudTrail/"
-    }
+#     filter {
+#       prefix = "${var.aws_organization_id}/AWSLogs/${var.aws_organization_id}/*/CloudTrail/"
+#     }
 
-    delete_marker_replication {
-      status = "Enabled"
-    }
-  }
-}
+#     delete_marker_replication {
+#       status = "Enabled"
+#     }
+#   }
+# }
 
 module "s3_org_config_logs" {
   providers = { aws = aws.hubandspoke }
@@ -818,7 +818,7 @@ data "aws_iam_policy_document" "config_log_delivery" {
   }
 }
 
-resource "aws_s3_bucket_replication_configuration" "org_config_logs" {
+resource "aws_s3_bucket_replication_configuration" "org_logs_to_hubandspoke" {
   provider = aws.log
 
   role   = aws_iam_role.org_logs_to_hubandspoke.arn
@@ -847,6 +847,36 @@ resource "aws_s3_bucket_replication_configuration" "org_config_logs" {
 
     filter {
       prefix = "${var.aws_organization_id}/AWSLogs/*/Config/"
+    }
+
+    delete_marker_replication {
+      status = "Enabled"
+    }
+  }
+
+  rule {
+    id     = "org-cloudtrail"
+    status = "Enabled"
+
+    destination {
+      bucket        = module.s3_org_cloudtrail_logs.s3_bucket_arn
+      storage_class = "STANDARD"
+      encryption_configuration {
+        replica_kms_key_id = aws_kms_key.hubandspoke_s3.arn
+      }
+    }
+
+    source_selection_criteria {
+      replica_modifications {
+        status = "Enabled"
+      }
+      sse_kms_encrypted_objects {
+        status = "Enabled"
+      }
+    }
+
+    filter {
+      prefix = "${var.aws_organization_id}/AWSLogs/${var.aws_organization_id}/*/CloudTrail/"
     }
 
     delete_marker_replication {
