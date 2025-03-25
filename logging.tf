58b21cec4bdda93566799bad165fe0dd5a9512fc
lancehampton <lance@stigian.com>
2025-03-25 07:45:31 -0400

    add debug replication events

--- logging.tf
+++ logging.tf
@@ -1137,3 +1137,109 @@ resource "aws_s3_bucket_replication_configuration" "ct_logs_replication" {
     }
   }
 }
+
+
+
+
+## Temp
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = module.s3_org_cloudtrail_logs.s3_bucket_id
+
+  queue {
+    queue_arn = aws_sqs_queue.repl.arn
+    events    = ["s3:Replication:OperationFailedReplication"]
+    # filter_suffix = ".log"
+  }
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = module.s3_org_config_logs.s3_bucket_id
+
+  queue {
+    queue_arn = aws_sqs_queue.repl.arn
+    events    = ["s3:Replication:OperationFailedReplication"]
+    # filter_suffix = ".log"
+  }
+}
+
+
+resource "aws_sqs_queue" "cega" {
+
+  name                       = "temp_repl_events"
+  visibility_timeout_seconds = 300 # 5 minutes minimum per Splunk docs
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.repl_dlq.arn
+    maxReceiveCount     = 4
+  })
+
+  # tags = local.tags
+}
+
+resource "aws_sqs_queue" "repl_dlq" {
+
+  name = "cega_dlq_splunk_events"
+}
+
+resource "aws_sqs_queue_redrive_allow_policy" "repl_dlq" {
+
+  queue_url = aws_sqs_queue.repl_dlq.id
+
+  redrive_allow_policy = jsonencode({
+    redrivePermission = "byQueue",
+    sourceQueueArns   = [aws_sqs_queue.repl.arn]
+  })
+}
+
+
+data "aws_iam_policy_document" "repl" {
+  statement {
+    sid     = "AllowS3ToSQS"
+    effect  = "Allow"
+    actions = ["SQS:SendMessage"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["s3.amazonaws.com"]
+    }
+
+    resources = [
+      "*"
+      # for key in keys(var.source_buckets) : aws_sqs_queue.events[key].arn,
+    ]
+
+    # condition {
+    #   test     = "ArnLike"
+    #   variable = "aws:SourceArn"
+    #   values = [
+    #     for key in keys(var.source_buckets) : data.aws_s3_bucket.source_buckets[key].arn
+    #   ]
+    # }
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [data.aws_caller_identity.log.account_id]
+    }
+  }
+
+  statement {
+    sid    = "AllowSQSToKMS"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["sqs.amazonaws.com"]
+    }
+    actions = [
+      "kms:GenerateDataKey",
+      "kms:Decrypt"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_sqs_queue_policy" "repl" {
+
+  queue_url = aws_sqs_queue.repl.id
+  policy    = data.aws_iam_policy_document.repl.json
+}
